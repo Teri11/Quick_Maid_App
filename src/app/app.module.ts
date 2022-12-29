279da5331eaf7d39e384ff5936a8b72226e611e6
Teri11 <augustineteri157@gmail.com>
2022-12-29 16:11:11 +0300

    added routing on navbar and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CommonModule } from './common/common.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    routingComponents,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
