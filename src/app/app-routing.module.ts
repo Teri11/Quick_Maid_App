281b1fc019febe6e8e856aa4c8d3507465a159bb
Teri11 <augustineteri157@gmail.com>
2022-12-29 15:42:18 +0300

    added routing to landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+const routes: Routes = [
+  {path: '', component:LandingPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  LandingPageComponent,
+
+];
